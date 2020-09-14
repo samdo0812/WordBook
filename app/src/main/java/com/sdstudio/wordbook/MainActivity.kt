e48078d696af967e7b8a77afce5ad11414a1ccaf
DoHoon <samdo0812@gmail.com>
2020-09-14 22:23:25 +0900

    word list fragment

--- app/src/main/java/com/sdstudio/wordbook/MainActivity.kt
+++ app/src/main/java/com/sdstudio/wordbook/MainActivity.kt
@@ -2,11 +2,19 @@ package com.sdstudio.wordbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = PagerAdapter(supportFragmentManager)
+        adapter.addFragment(fragment1(), "My Word")
+        adapter.addFragment(fragment2(), "Setting")
+        viewPager.adapter = adapter
+        tabLayout.setupWithViewPager(viewPager)
+
     }
 }
