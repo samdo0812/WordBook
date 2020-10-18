3cc567517b193b80e547d61cf57d50d631259e09
DoHoon <samdo0812@gmail.com>
2020-10-18 18:24:29 +0900

    viewPager2 recyclerview

--- app/src/main/java/com/sdstudio/wordbook/addWordAdapter.kt
+++ app/src/main/java/com/sdstudio/wordbook/addWordAdapter.kt
@@ -3,10 +3,15 @@ package com.sdstudio.wordbook
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.word_fragment_item.view.*
+import java.util.*
 
-class addWordAdapter(val list: List <data>?) : RecyclerView.Adapter<Holder>() {
+//class addWordAdapter(val list: List <data>?) : RecyclerView.Adapter<Holder>() {
+class addWordAdapter(val list: ArrayList<data>) : RecyclerView.Adapter<Holder>() {
+
+    //var list = ArrayList<data>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view = LayoutInflater.from(parent.context)
@@ -15,17 +20,23 @@ class addWordAdapter(val list: List <data>?) : RecyclerView.Adapter<Holder>() {
     }
 
     override fun getItemCount(): Int {
-        return list!!.size
+        return list.size
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.bind(list!!.get(position),position)
+        holder.word.text = list.get(position).Word
+        holder.mean.text = list.get(position).Mean
     }
+
+    /*fun setArraylist(list:ArrayList<data>){
+        this.list = list
+    }*/
 }
 
 class Holder(itemView: View): RecyclerView.ViewHolder(itemView){
-    fun bind(data: data, position: Int){
-        itemView.word.setText(data.Word)
-        itemView.mean.setText(data.Mean)
-    }
+    val word = itemView.findViewById<TextView>(R.id.word)
+    val mean = itemView.findViewById<TextView>(R.id.mean )
+
+
+
 }
--- app/src/main/java/com/sdstudio/wordbook/word_fragment.kt
+++ app/src/main/java/com/sdstudio/wordbook/word_fragment.kt
@@ -2,10 +2,11 @@ package com.sdstudio.wordbook
 
 import android.app.AlertDialog
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.add_word.*
@@ -13,9 +14,18 @@ import kotlinx.android.synthetic.main.add_word.view.*
 import kotlinx.android.synthetic.main.fragment_word.*
 import kotlinx.android.synthetic.main.fragment_word.view.*
 import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.log
 
 
 class word_fragment : Fragment() {
+    //lateinit var addword:String
+    //lateinit var addmean:String
+    lateinit var adapter: addWordAdapter
+
+    //fun setArrayList(list: ArrayList<data>){
+      //  this.list = list
+    //}
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,32 +34,53 @@ class word_fragment : Fragment() {
     ): View? {
         val view = inflater.inflate(com.sdstudio.wordbook.R.layout.fragment_word, container, false)
 
-        // val adapter = addWordAdapter()
-        // view.recyclerView.adapter = adapter
-        //view.recyclerView.layoutManager = LinearLayoutManager(context)
 
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
-        //var dialog = dialog()
+        val list = ArrayList<data>()
+        //플로팅버튼
         floatingButton.setOnClickListener {
             val mDialog = LayoutInflater.from(this.context).inflate(R.layout.add_word,null)
-            val mbuilder = AlertDialog.Builder(this.context).setView(mDialog )
+            val mbuilder = AlertDialog.Builder(this.context).setView(mDialog)
             val myAlertDialog = mbuilder.show()
 
+            //다이얼로그 열기(단어등록)
             mDialog.addwordButton.setOnClickListener {
+                val addword = mDialog.addword.text.toString()
+                val addmean = mDialog.addmean.text.toString()
 
+                //다이얼로그에 입력한 값
+                //list =  arrayListOf(data(addword,addmean))
+                list.add(data(addword,addmean))
+                //Log.d("test",list.toString())
+
+                //recyclerView.layoutManager = LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false)
+                //recyclerView.adapter = addWordAdapter(list)
+
+                adapter = addWordAdapter(list)
+                adapter.notifyDataSetChanged()
+                recyclerView.adapter = adapter
+                view.recyclerView.layoutManager = LinearLayoutManager(context)
+                //setArrayList(list)
+                myAlertDialog.dismiss()
             }
+
+            //다이얼로그 취소
             mDialog.addwordCancel.setOnClickListener {
                 myAlertDialog.dismiss()
             }
         }
+
+
     }
 }
+
+
+
+
 
 
 
